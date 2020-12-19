4aa77019fd327b19360d93080c46ddd12480fa14
josephden16 <tegajoseph2002@gmail.com>
2020-12-19 08:01:33 +0100

    finish countdwon and user-interface

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-var seconds = 60;
-var secondsCountdown = setInterval(function () {
-    if (seconds < 1) {
-        seconds = 60;
-    }
-    else {
-        seconds--;
-        console.log("seconds - " + seconds);
-    }
-}, 1000);
-var minute = 59;
-var miuteCountdown = setInterval(function () {
-    if (minute < 1) {
-        clearInterval(miuteCountdown);
-        return;
-    }
-    else {
-        minute--;
-        console.log("minute - " + minute);
-    }
-}, 1000 * 60);
+"use strict";
+var seconds = document.getElementById("seconds");
+var minute = document.getElementById("minutes");
+var hours = document.getElementById("hours");
+var days = document.getElementById("days");
+// Countdown Date set to the day this code was published
+var countDownDate = new Date(2021, 0, 2).getTime();
+// reset the time when the page loads
+window.onload = function () {
+    // Timer
+    var countdown = setInterval(function () {
+        var now = new Date().getTime();
+        var timeleft = countDownDate - now;
+        if (timeleft < 0) {
+            seconds.textContent = "0";
+            minute.textContent = "0";
+            hours.textContent = "0";
+            days.textContent = "0";
+            clearInterval(countdown);
+            return;
+        }
+        var d = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+        var h = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var m = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+        var s = Math.floor((timeleft % (1000 * 60)) / 1000);
+        seconds.textContent = s;
+        minute.textContent = m;
+        hours.textContent = h;
+        days.textContent = d;
+    }, 1000);
+};
--- script.ts
+++ script.ts
@@ -1,21 +1,33 @@
+let seconds: any = document.getElementById("seconds");
+let minute: any = document.getElementById("minutes");
+let hours: any = document.getElementById("hours");
+let days: any = document.getElementById("days");
 
-let seconds: number = 60;
-let secondsCountdown = setInterval(function () {
-  if (seconds < 1) {
-    seconds = 60;
-  } else {
-    seconds--;
-    console.log("seconds - " + seconds);
-  }
-}, 1000);
+// Countdown Date set to the day this code was published
+let countDownDate: number = new Date(2021, 0, 2).getTime();
 
-let minute: number = 59;
-let miuteCountdown = setInterval(function () {
-  if (minute < 1) {
-    clearInterval(miuteCountdown);
-    return;
-  } else {
-    minute--;
-    console.log("minute - " + minute);
-  }
-}, 1000 * 60);
+// reset the time when the page loads
+window.onload = function () {
+  // Timer
+  let countdown = setInterval(() => {
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+    if (timeleft < 0) {
+      seconds.textContent = "0";
+      minute.textContent = "0";
+      hours.textContent = "0";
+      days.textContent = "0";
+      clearInterval(countdown);
+      return;
+    }
+    var d = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var h = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+    seconds.textContent = s;
+    minute.textContent = m;
+    hours.textContent = h;
+    days.textContent = d;
+  }, 1000);
+}
